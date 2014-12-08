c9b86e37b7e55a8d9ad10e6f063c179dd1b60a3b
Ben Purdy <ben.purdy@gmail.com>
2014-12-07 18:31:52 -0800

    Adding files.

---
+++ GruntFile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: ['lib/*.js', 'src/*.js'],
+        dest: 'build/maptastic.js',
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'build/maptastic.js',
+        dest: 'build/maptastic.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('dev', ['concat']);
+
+};
